8a8e17d3eb6957b288e3c5766cc639df49a6006d
Hal Finkel <hfinkel@anl.gov>
2019-12-28 00:39:53 +0000

    an initial impl which compiles

--- clang/include/clang/Parse/Parser.h
+++ clang/include/clang/Parse/Parser.h
@@ -473,6 +473,10 @@ public:
   ///
   void Initialize();
 
+  /// Add to the string of predefines in the preprocessor from the registered
+  //syntax plugins.
+  void AddPluginPredefines();
+
   /// Parse the first top-level declaration in a translation unit.
   bool ParseFirstTopLevelDecl(DeclGroupPtrTy &Result,
                               Sema::ModuleImportState &ImportState);
@@ -1620,6 +1624,7 @@ private:
                                                 AccessSpecifier AS);
 
   void SkipFunctionBody();
+  void ProcessPluginSyntax(ParsingDeclarator &D);
   Decl *ParseFunctionDefinition(ParsingDeclarator &D,
                  const ParsedTemplateInfo &TemplateInfo = ParsedTemplateInfo(),
                  LateParsedAttrList *LateParsedAttrs = nullptr);
@@ -3673,8 +3678,10 @@ public:
   virtual ~SyntaxHandler();
 
   StringRef getName() const { return Name; }
-  virtual void HandlePragma(Preprocessor &PP, PragmaIntroducer Introducer,
-                            Token &FirstToken) = 0;
+  virtual void GetReplacement(Preprocessor &PP, Declarator &D,
+                              CachedTokens &Toks,
+                              llvm::raw_string_ostream &OS) = 0;
+  virtual void AddToPredefines(llvm::raw_string_ostream &OS) = 0;
 };
 
 /// Registry of syntax handlers added by plugins
